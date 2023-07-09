69a7c341fd98932ed75005dab5918f66b11d72e8
r.pakhomov <calvinhobbeson.java@gmail.com>
2023-07-09 13:25:11 +0300

    8. Скачивание файла с ограничением. [#144271 #425986]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -1,9 +1,8 @@
 package ru.job4j.thread;
 
-import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
+import java.io.IOException;
 
 public class Wget implements Runnable {
     private final String url;
@@ -17,16 +16,27 @@ public class Wget implements Runnable {
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream(url + ".temp")) {
-            byte[] dataBuffer = new byte[1024];
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1048576];
             int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+            int downloadData = 0;
+            long start = System.currentTimeMillis();
+            long stop = 0;
+            while ((bytesRead = in.read(dataBuffer, 0, 1048576)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                Thread.sleep(1000);
+                downloadData += bytesRead;
+                if (downloadData >= speed) {
+                    long downloadTime = System.currentTimeMillis() - start;
+                    if (downloadTime < 1000) {
+                        Thread.sleep(1000 - downloadTime);
+                    }
+                    downloadData = 0;
+                    start = System.currentTimeMillis();
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
+        }catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
@@ -35,6 +45,9 @@ public class Wget implements Runnable {
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget(url, speed));
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Parameters added incorrectly");
+        }
         wget.start();
         wget.join();
     }
