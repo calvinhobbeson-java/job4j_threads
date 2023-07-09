46ff780b231facf08aa1e25856be43683470587c
r.pakhomov <calvinhobbeson.java@gmail.com>
2023-07-09 13:30:38 +0300

    8. Скачивание файла с ограничением. [#144271 #425986]

--- src/main/java/ru/job4j/thread/FileDownload.java
+++ src/main/java/ru/job4j/thread/FileDownload.java
@@ -6,15 +6,14 @@ import java.sql.Timestamp;
 
 public class FileDownload {
     public static void main(String[] args) throws Exception {
-        String file = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
-        try (BufferedInputStream in = new BufferedInputStream(new URL(file).openStream());
+        try (BufferedInputStream in = new BufferedInputStream(new URL(args[1]).openStream());
              FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
             long start = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                //Thread.sleep(1000);
+                Thread.sleep(1000);
             }
             long stop = System.currentTimeMillis();
             System.out.println(stop - start);
