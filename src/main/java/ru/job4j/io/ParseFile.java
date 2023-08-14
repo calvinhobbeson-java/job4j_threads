e24281d0e629285326662ad05c269f016c537538
Ruviel21 <calvinhobbeson.java@gmail.com>
2023-08-14 19:10:49 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -12,18 +12,18 @@ public class ParseFile {
 
     public String content(Predicate<Integer> filter) throws IOException {
         BufferedInputStream i = new BufferedInputStream(new FileInputStream(file));
-        String output = "";
+        StringBuilder output = new StringBuilder();
         int data;
         try {
-            while ((data = i.read()) > 0) {
+            while ((data = i.read()) != -1) {
                 if (filter.test(data)) {
-                    output += (char) data;
+                    output.append((char) data);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return output;
+        return output.toString();
     }
 
     public String getContentWithoutUnicode() throws IOException {
