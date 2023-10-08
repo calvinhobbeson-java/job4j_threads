5a7e74e95efb9657493df924dac199ed574ad2fe
r.pakhomov <calvinhobbeson.java@gmail.com>
2023-10-08 22:20:58 +0600

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ src/main/java/ru/job4j/concurrent/SimpleBlockingQueue.java
@@ -0,0 +1,35 @@
+package ru.job4j.concurrent;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+    private final int limit;
+
+    public SimpleBlockingQueue(int limit){
+        this.limit = limit;
+    }
+
+    public void offer(T value) throws InterruptedException {
+        while (queue.size() == limit) {
+            wait();
+        }
+        queue.add(value);
+        notifyAll();
+    }
+
+    public T poll() throws InterruptedException {
+        while (queue.size() == 0) {
+            wait();
+        }
+        notifyAll();
+        return queue.poll();
+    }
+}
---
+++ src/test/java/ru/job4j/concurrent/SimpleBlockingQueueTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.concurrent;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleBlockingQueueTest {
+
+    SimpleBlockingQueue simpleQueue = new SimpleBlockingQueue(3);
+
+    @Test
+    public void whenAllWorksFine() throws InterruptedException {
+        simpleQueue.offer(1);
+        simpleQueue.offer(2);
+        simpleQueue.offer(3);
+        Thread producer = new Thread(
+                () -> {
+                    try {
+                        simpleQueue.offer(4);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
+        Thread consumer = new Thread(
+                () -> {
+                    try {
+                        simpleQueue.poll();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+        assertEquals(simpleQueue.poll(),3);
+    }
+}
