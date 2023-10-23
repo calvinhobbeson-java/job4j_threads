579301692b57959929c9a5616be39541560ceb18
r.pakhomov <calvinhobbeson.java@gmail.com>
2023-10-23 11:56:29 +0600

    1. Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,27 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        return memory.computeIfPresent(model.getId(), (a, b) -> {
+        if (model.getVersion() != b.getVersion()) {
+            throw new OptimisticException("Versions are not equal");
+        }
+        Base updatedModel = new Base(model.getId(), model.getVersion() + 1);
+        updatedModel.setName(model.getName());
+        return updatedModel;
+    }) != null;
+    }
+
+    public void delete(Base model) {
+        memory.remove(model.getId());
+    }
+}
---
+++ src/test/java/ru/job4j/cache/CacheTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.cache;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CacheTest {
+    @Test
+    void whenUpdateThenUpdateCorrectly() {
+    Base base = new Base(1, 0);
+    Cache cache  = new Cache();
+    base.setName("base1");
+    cache.add(base);
+    base.setName("base2");
+    cache.update(base);
+    assertEquals(base.getName(), "base2");
+}
+    @Test
+    void whenDeleteThenDeleteCorrectly() {
+        Base base = new Base(1, 0);
+        Cache cache  = new Cache();
+        cache.add(base);
+        cache.delete(base);
+        assertNull(base.getId());
+    }
+
+}
