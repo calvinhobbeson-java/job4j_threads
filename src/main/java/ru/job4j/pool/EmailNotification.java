b1260cb7198a5112968ad80e602e2190f2963ca2
r.pakhomov <calvinhobbeson.java@gmail.com>
2023-11-13 22:22:33 +0600

    2. ExecutorService рассылка почты. [#63097]

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -1,32 +1,25 @@
 package ru.job4j.pool;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification {
 
-    private ExecutorService pool;
-
-    public EmailNotification(ExecutorService pool) {
-        this.pool = pool;
-    }
+    private ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public void send(String subject, String body, String email) {
 
     }
 
-    public List<String> emailTo(User user) {
-        ArrayList<String> message = new ArrayList<>();
-        String username = user.getUsername();
-        String email = user.getEmail();
-        String subject = String.format("Notification %s to email %s", username, email);
-        String body = String.format("Add a new event for user %s", username);
-        message.add(subject);
-        message.add(body);
-        message.add(email);
-        return message;
+    public void emailTo(User user) {
+        String subject = String.format("Notification %s to email %s", user.getUsername(), user.getEmail());
+        String body = String.format("Add a new event for user %s", user.getUsername());
+        pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                send(subject, body, user.getEmail());
+            }
+        });
     }
 
     public void close() {
@@ -38,19 +31,5 @@ public class EmailNotification {
                 e.printStackTrace();
             }
         }
-    }
-
-    public static void main(String[] args) {
-        EmailNotification emailNotification = new EmailNotification(Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()));
-        emailNotification.pool.submit(new Runnable() {
-            @Override
-            public void run() {
-                User user = new User();
-                user.setEmail("Oleg1234@gmail.com");
-                user.setUsername("OlegBanzai");
-                emailNotification.send(emailNotification.emailTo(user).get(0), emailNotification.emailTo(user).get(1), emailNotification.emailTo(user).get(2));
-            }
-        });
-        emailNotification.pool.shutdown();
     }
 }
