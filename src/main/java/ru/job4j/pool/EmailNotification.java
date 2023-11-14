604310e40c7066e8d4f6337bf0888aca1b2df66a
r.pakhomov <calvinhobbeson.java@gmail.com>
2023-11-14 12:27:40 +0600

    2. ExecutorService рассылка почты. [#63097]

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -14,12 +14,7 @@ public class EmailNotification {
     public void emailTo(User user) {
         String subject = String.format("Notification %s to email %s", user.getUsername(), user.getEmail());
         String body = String.format("Add a new event for user %s", user.getUsername());
-        pool.submit(new Runnable() {
-            @Override
-            public void run() {
-                send(subject, body, user.getEmail());
-            }
-        });
+        pool.submit(() -> send(subject, body, user.getEmail()));
     }
 
     public void close() {
