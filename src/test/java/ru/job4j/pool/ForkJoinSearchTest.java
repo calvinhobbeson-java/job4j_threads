e8c813a1bfe893708c80f3e432a5bd9c3ae546ec
r.pakhomov <calvinhobbeson.java@gmail.com>
2023-12-02 22:12:07 +0300

    3. ForkJoinPool [#315067]

--- src/test/java/ru/job4j/pool/ForkJoinSearchTest.java
+++ src/test/java/ru/job4j/pool/ForkJoinSearchTest.java
@@ -9,8 +9,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class ForkJoinSearchTest {
     @Test
     public void whenLinearSearch()  {
-        Object[] array = new Integer[] {1, 2, 3, 4, 5, 6, 7, 1, 8, 6};
+        Object[] array = {1, 2, 3, 4, 5, 6, 7, 1, 8};
         int result = ForkJoinSearch.search(array, 8);
         assertEquals(result, 8);
+    }
+
+    @Test
+    public void whenParallelSearch() {
+        Object[] array = {1, "phrase", 2, 6, 5, 2, 4, 5, 6, 7, 4, 6, 18};
+        int result = ForkJoinSearch.search(array, 18);
+        assertEquals(result, 18);
+    }
+
+    @Test
+    public void whenDifferentTypes() {
+        Object[] array = {1, "phrase", 2, 6, 5, 2, 4, 5, 6, 7, 4, 6, 18};
+        int result = ForkJoinSearch.search(array, "phrase");
+        assertEquals(result, "phrase");
+    }
+
+    @Test
+    public void whenItemNotFound() {
+        Object[] array = {1, "phrase", 2, 6, 5, 2, 4, 5, 6, 7, 4, 6, 18};
+        int result = ForkJoinSearch.search(array, "phrases");
+        assertEquals(result, -1);
     }
 }
