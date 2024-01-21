4f9902b06acd48acfb709e89d02689baca4eb1a3
Ruviel21 <calvinhobbeson.java@gmail.com>
2024-01-21 20:08:53 +0300

    4. CompletableFuture [#361626]

--- src/main/java/ru/job4j/pools/RolColSum.java
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -1,5 +1,8 @@
 package ru.job4j.pools;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 public class RolColSum {
     public static class Sums {
         private int rowSum;
@@ -23,21 +26,45 @@ public class RolColSum {
     }
 
     public static Sums[] sum(int[][] matrix) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        Sums[] allSums = new Sums[rows];
+        int sum = 0;
         Sums sums = new Sums();
-        int rowSum = 0;
-        int colSum = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                rowSum += matrix[i][j];
-                colSum += matrix[j][i];
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    sum += matrix[i][j];
+                    sums.setRowSum(sum);
+                    allSums[i] = sums;
+                }
             }
+            for (int i = 0; i < columns; i++) {
+                for (int j = 0; j < rows; j++) {
+                    sum += matrix[i][j];
+                    sums.setColSum(sum);
+                    allSums[i] = sums;
+                }
+            }
+        return allSums;
         }
-        sums.setColSum(colSum);
-        sums.setRowSum(rowSum);
+
+    public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        Sums[] allSums = new Sums[rows];
+        int sum = 0;
+        Sums sums = new Sums();
     }
-
-    public static Sums[] asyncSum(int[][] matrix) {
-
+        public static CompletableFuture<Integer> getTask(int[][] data) {
+            return CompletableFuture.supplyAsync(() -> {
+                for (int i = 0; i < rows; i++) {
+                    for (int j = 0; j < columns; j++) {
+                        sum += matrix[i][j];
+                        sums.setRowSum(sum);
+                        allSums[i] = sums;
+                    }
+                }
+            }
     }
 
 }
