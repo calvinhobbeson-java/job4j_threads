6f661d2d5147f8d188735050b16ef2877abc08d1
Ruviel21 <calvinhobbeson.java@gmail.com>
2024-01-25 20:08:14 +0300

    4. CompletableFuture [#361626]

--- src/main/java/ru/job4j/pools/RolColSum.java
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -6,44 +6,19 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 public class RolColSum {
-    public static class Sums {
-        private int rowSum;
-        private int colSum;
-
-        public int getRowSum() {
-            return rowSum;
-        }
-
-        public void setRowSum(int rowSum) {
-            this.rowSum = rowSum;
-        }
-
-        public int getColSum() {
-            return colSum;
-        }
-
-        public void setColSum(int colSum) {
-            this.colSum = colSum;
-        }
-    }
 
     public static Sums[] sum(int[][] matrix) {
         int rows = matrix.length;
-        int columns = matrix[0].length;
         Sums[] allSums = new Sums[rows];
-        int sum = 0;
-        Sums sums = new Sums();
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                sum += matrix[i][j];
-                sums.setRowSum(sum);
-                allSums[i] = sums;
-            }
-        }
-        for (int i = 0; i < columns; i++) {
+            int rowSum = 0;
+            int colSum = 0;
+            Sums sums = new Sums();
             for (int j = 0; j < rows; j++) {
-                sum += matrix[i][j];
-                sums.setColSum(sum);
+                rowSum += matrix[i][j];
+                colSum += matrix[j][i];
+                sums.setRowSum(rowSum);
+                sums.setRowSum(colSum);
                 allSums[i] = sums;
             }
         }
